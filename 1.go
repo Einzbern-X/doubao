4e7d1d98ae464605a293fe9c28f2cb0b9c681791
Einzbern-X <18522640299@163.com>
2026-02-04 22:55:55 +0800

    first change

---
+++ 1.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!你好")
+	fmt.Println("こんにちは世界")
+	fmt.Println("안녕하세요 세계")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Einzbern-X/demo1
+
+go 1.25.6
